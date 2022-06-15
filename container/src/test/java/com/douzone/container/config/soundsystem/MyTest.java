3d5500911b518776d16cae2bc478e6736f069c4d
chaewon <rlacodnjs98@naver.com>
2022-06-15 14:15:06 +0900

    Auto Configuration(Annotation Scanning)

---
+++ container/src/test/java/com/douzone/container/config/soundsystem/MyTest.java
@@ -0,0 +1,20 @@
+package com.douzone.container.config.soundsystem;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class MyTest {
+	
+	@Test
+	public void test01() {
+		assertTrue(true);
+	}
+	
+	@Test
+	public void test02() {
+		int i = 1 + 2;
+		assertEquals(3, i);
+	}
+}
